94422b76f85562df7957fcbb0b0f2b4255df76a7
littlecapa <littlecapa@googlemail.com>
2025-11-17 14:57:12 +0100

    Logging in startup.sh included

--- startup.sh
+++ startup.sh
@@ -1,6 +1,22 @@
-# startup.sh is used by infra/resources.bicep to automate database migrations and isn't used by the sample application
-python manage.py migrate
-python manage.py collectstatic --noinput
-gunicorn --workers 2 --threads 4 --timeout 60 --access-logfile \
-    '-' --error-logfile '-' --bind=0.0.0.0:8000 \
-     --chdir=/home/site/wwwroot azureproject.wsgi
+#!/bin/bash
+
+# Exit on any error
+set -e
+
+echo "=== Starting Django Application ==="
+
+# Run database migrations
+echo "Running migrations..."
+python manage.py migrate --noinput
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput --clear
+
+# Start Gunicorn
+echo "Starting Gunicorn..."
+gunicorn --workers 2 --threads 4 --timeout 60 \
+    --access-logfile '-' --error-logfile '-' \
+    --bind=0.0.0.0:8000 \
+    --chdir=/home/site/wwwroot \
+    azureproject.wsgi:application
