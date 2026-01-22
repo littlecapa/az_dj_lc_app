4c66eca3edc4a30506a64a649d48d3c886087137
littlecapa <littlecapa@googlemail.com>
2026-01-22 12:06:59 +0100

    Serve Import vergessen

--- azureproject/urls.py
+++ azureproject/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
+from django.views.static import serve
 
 urlpatterns = [
     path('', include('homepage.urls')),
