059b5339b8b5e6f2baa9ab9f7dde5414254ec8ab
littlecapa <littlecapa@googlemail.com>
2026-01-29 09:08:21 +0100

    updated security settings

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -195,3 +195,15 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Wichtig: Domain auf None lassen, damit es für alle Domains (azurewebsites.net UND littlecapa.com) gilt
+SESSION_COOKIE_DOMAIN = None 
+CSRF_COOKIE_DOMAIN = None
+
+# Da Sie jetzt HTTPS nutzen, muss das zwingend True sein:
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+
+# Verhindert, dass Cookies in iframes geladen werden (Sicherheit)
+CSRF_COOKIE_SAMESITE = 'Lax'
+SESSION_COOKIE_SAMESITE = 'Lax'
