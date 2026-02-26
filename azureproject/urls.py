75541b372e7cc72ddbf037ff35102532ab6cff3e
littlecapa <littlecapa@googlemail.com>
2026-02-26 17:21:14 +0100

    robots.txt Handling eingefügt

--- azureproject/urls.py
+++ azureproject/urls.py
@@ -1,27 +1,27 @@
-"""azureproject URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path, re_path
 from django.views.static import serve
+from django.http import HttpResponse
 
 urlpatterns = [
     path('', include('homepage.urls')),
     path('fintech/', include('fintech.urls')),
     path('admin/', admin.site.urls),
+    path('robots.txt', 
+         lambda request: HttpResponse(
+             "User-agent: *\nAllow: /\n\n"
+             "Sitemap: https://littlecapa.com/sitemap.xml", 
+             content_type='text/plain'
+         ), 
+         name='robots_txt'
+    ),
+    path('sitemap.xml', 
+         lambda request: HttpResponse(
+             '''<?xml version="1.0"?>
+             <urlset><url><loc>https://littlecapa.com/</loc></url></urlset>''',
+             content_type='application/xml'
+         )),
 ]
 
 # Standard-Weg für Development (DEBUG=True)
