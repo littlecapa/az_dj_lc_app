c208eb9ea8905426d1abca3cbdf7365c73d23521
littlecapa <littlecapa@googlemail.com>
2026-03-16 14:37:56 +0100

    Sortierreihenfolge BP Messungen geändert

--- health/views.py
+++ health/views.py
@@ -5,17 +5,18 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 
 import csv
 
-class BloodPressureListView(LoginRequiredMixin, ListView):  # class, kein def!
+class BloodPressureListView(LoginRequiredMixin, ListView):
     model = BloodPressure
     template_name = 'health/bp_list.html'
     context_object_name = 'messungen'
+    ordering = ['-datum_zeit']
 
 def get_export_csv(request):  # Diese Funktion ist OK, ohne as_view
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="blutdruck.csv"'
     writer = csv.writer(response)
     writer.writerow(['Datum/Uhrzeit', 'Systolisch', 'Diastolisch', 'Herzschlag', 'Info'])
-    for messung in BloodPressure.objects.order_by('-datum'):
+    for messung in BloodPressure.objects.order_by('-datum_zeit'):
         writer.writerow([messung.datum_zeit.strftime('%d.%m.%Y %H:%M'), messung.systolisch, messung.diastolisch, 
                          messung.herzschlag, 'Ja' if messung.info else 'Nein'])
     return response
